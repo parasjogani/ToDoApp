c1f14ee2c4b2d07aa3a603b7a489b6be9da07b26
parasjogani <paras.jogani@gmail.com>
2022-12-15 22:24:39 +0530

    some changes

--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -14,6 +14,21 @@ const TodoList = () => {
         }
     };
 
+    //Add Task
+    const handleAddTask = async (todo) => {
+      const newTask = prompt("Enter new task")
+
+      if (!newTask) {
+        alert("Please enter your task")
+      } else {
+        const resp = await axios.post(`/createTodos/${todo._id}`, {
+          text: newTask
+        });
+        console.log(resp);
+      }
+
+    }
+
     //Edit
 
     const handleEdit = async (todo) => {
@@ -36,6 +51,12 @@ const TodoList = () => {
       console.log(resp);
     }
 
+    //Delete Task
+    const handleDeleteTask = async (todo) => {
+      const resp = await axios.delete(`/deleteTask/${todo._id}`)
+      console.log(resp);
+    }
+
     useEffect(() => {
         fetchtodoData();
     }, [todoData]);
@@ -50,14 +71,21 @@ const TodoList = () => {
     
     <div className="flex flex-wrap -m-2">
           {todoData && todoData.map((todo) => (
-            <div className="p-2 lg:w-1/3 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border bg-sky-200 p-4 rounded-lg">
+            <div className="p-2 lg:w-1/2 md:w-1/2 w-full">
+              <div className="h-full items-center border-gray-200 border bg-sky-200 p-4 rounded-lg">
                 <div className="flex-grow flex justify-between">
-                  <h2 className="text-gray-900 title-font font-medium">{todo.title}</h2>
+                  <h2 className="sm:text-xl text-lg text-gray-900 title-font font-medium">{todo.title}</h2>
                   <div className="space-x-4">
+                    <button className="bg-neutral-700 px-5 py-1 rounded-md text-white" onClick={() => handleAddTask(todo)}>Add Task</button>
                     <button className="bg-green-500 px-5 py-1 rounded-md text-white" onClick={() => handleEdit(todo)}>Edit</button>
                     <button className="bg-red-500 px-3 py-1 rounded-md text-white" onClick={() => handleDelete(todo._id)}>Delete</button>
                   </div>
+                </div>
+                <div >
+                  {todo.tasks.map((val) => 
+                    <li>{val}<button class="rounded-full bg-red-700 text-white" onClick={() => handleDeleteTask(todo)}>X</button></li>
+
+                    )}
                 </div>
               </div>
             </div>
