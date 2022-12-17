b6d06828c6b2df0f4c9ffd1dec7d4164c3e351ce
parasjogani <paras.jogani@gmail.com>
2022-12-17 23:04:51 +0530

    update UI

--- frontend/src/components/TitleForm.jsx
+++ frontend/src/components/TitleForm.jsx
@@ -25,25 +25,23 @@ const TitleForm = () => {
   return (
     <>
         <div className="flex flex-col max-w p-6 rounded-md sm:p-10">
-	<div className="mb-8 text-center">
-		<h1 className="my-3 text-4xl font-bold">Welcome to TODO APP</h1>
-		
-	</div>
-	<form onSubmit={handleSubmit}>
-		<div className="flex justify-center align-center gap-3">
-			<div>
-				<input type="text" name="title" id="title" placeholder="Input your title here" className="w-[280px] px-3 py-2 border rounded-md dark:border-gray-700 dark:text-gray-700"
-                value={todoTitle} onChange={(event) => settodoTitle(event.target.value)}
-                />
-			</div>
-
-			<div>
-				<button type="submit" className="px-6 py-2 font-semibold rounded-md border">Submit</button>
-			</div>
-		</div>
-	</form>
-</div>
-</>
+	        <div className="mb-8 text-center">
+		        <h1 className="my-3 text-4xl font-bold">Welcome to TODO APP</h1>
+	        </div>
+	        <form onSubmit={handleSubmit}>
+		        <div className="flex justify-center align-center gap-3">
+			        <div>
+				        <input type="text" name="title" id="title" placeholder="Input your title here" className="w-[280px] px-3 py-2 border rounded-md dark:border-gray-700 dark:text-gray-700"
+                        value={todoTitle} onChange={(event) => settodoTitle(event.target.value)}
+                        />
+			        </div>
+			        <div>
+				        <button type="submit" className="px-6 py-2 font-semibold rounded-md border">Submit</button>
+			        </div>
+		        </div>
+	        </form>
+        </div>
+    </>
     
   )
 }
--- frontend/src/components/TodoList.jsx
+++ frontend/src/components/TodoList.jsx
@@ -55,6 +55,7 @@ const TodoList = () => {
             newVal: newTask,
             index: val
         })
+        console.log(resp);
       }
     }
 
@@ -68,9 +69,7 @@ const TodoList = () => {
     const handleDeleteTask = async (todo, val) => {
       const resp = await axios.delete(`/deleteTask/${todo._id}`,
       {name: val} 
-
       )
-      
       console.log(resp);
     }
 
@@ -100,9 +99,25 @@ const TodoList = () => {
                 </div>
                 <div >
                   {todo.tasks.map((val, index) => 
-                    <li>{val}
-                    <button class="rounded-full bg-green-700 text-white" onClick={() => handleEditTask(todo, index)}>E</button>
-                    <button class="rounded-full bg-red-700 text-white" onClick={() => handleDeleteTask(todo, index)}>X</button></li>
+                    <li className="flex items-center px-5 ">{val}
+                    <div className="items-center px-3">
+                      <button class="hover:text-green-600 bg-transparent font-medium px-2 py-1 inline-flex" onClick={() => handleEditTask(todo, index)}>
+                        <span>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                          </svg>
+                        </span>
+                      </button>
+
+                      <button class="hover:text-red-700 bg-transparent font-medium px-2 py-1 inline-flex" onClick={() => handleDeleteTask(todo, index)}>
+                        <span>
+                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                          </svg>
+                        </span>
+                      </button>
+                    </div>
+                    </li>
 
                     )}
                 </div>
